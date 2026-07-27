158b0c1d353c248d1a8aeb97a7cd67880b1ff833
Mohamed Lukman <mdlukman0204@gmail.com>
2026-07-27 17:04:04 +0530

    Implement digit sum calculation for even and odd digits
    This program calculates the sum of even and odd digits from a user-input number, displaying the results separately.

---
+++ Sum of Even and Odd Digits.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SumEvenOddDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit % 2 == 0)
+                evenSum += digit;
+            else
+                oddSum += digit;
+
+            num /= 10;
+        }
+
+        System.out.println("Even Digit Sum = " + evenSum);
+        System.out.println("Odd Digit Sum = " + oddSum);
+    }
+}
